470b0c298a7125d23c1fe785515dc431696b2b1b
Y.Takeuchi <kaidouji85@gmail.com>
2024-06-23 13:24:03 +0900

    フロントをamplifyのサンプルに置き換えた

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,27 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Todo App</title>
+</head>
+<body>
+<main>
+  <h1>My todos</h1>
+  <button id="addTodo">+ new</button>
+  <ul id="todoList"></ul>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    Try creating a new todo.
+    <br>
+    <a href="https://docs.amplify.aws/javascript/start/quickstart/">
+      Review next step of this tutorial.
     </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
   </div>
+</main>
+<script type="module" src="src/main.ts"></script>
+</body>
+</html>
 `
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
